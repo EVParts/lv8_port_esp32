a382c1988d411b1fa0f483c1dbae018f05c01f7f
MacDue <macdue@dueutil.tech>
2022-02-28 12:30:40 +0000

    Add LV init semaphore

--- main/main.cpp
+++ main/main.cpp
@@ -2,6 +2,7 @@
 #include <esp_event.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <freertos/semphr.h>
 
 #include "can.h"
 #include "lvgl_esp.h"
@@ -11,6 +12,8 @@
 constexpr auto LOG_TAG = "Leaf Can";
 constexpr gpio_num_t CAN_TX_PIN = GPIO_NUM_32;
 constexpr gpio_num_t CAN_RX_PIN = GPIO_NUM_27;
+
+static SemaphoreHandle_t g_lv_init_sema = nullptr;
 
 static struct leaf_dash {
     dash_t* handle;
@@ -39,11 +42,19 @@ extern "C" void lv_esp_user_gui_init() {
         g_leaf_dash.handle , "Energy Left", "%d Gids");
 
     (void) lv_timer_create(leaf_dash_update, DISPLAY_UPDATE_INTERVAL_MS, nullptr);
+    xSemaphoreGive(g_lv_init_sema);
 }
 
-extern "C" void app_main() { 
+extern "C" void app_main() {
     ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    g_lv_init_sema = xSemaphoreCreateBinary();
+    assert(g_lv_init_sema);
     ESP_ERROR_CHECK(lv_esp_init());
+    xSemaphoreTake(g_lv_init_sema, portMAX_DELAY);
+    vSemaphoreDelete(g_lv_init_sema);
+    g_lv_init_sema = nullptr;
+
     CANBus can_bus(CAN_TX_PIN, CAN_RX_PIN);
     ESP_ERROR_CHECK(can_bus.start());
 
@@ -52,7 +63,7 @@ extern "C" void app_main() {
         if (can_bus.wait_for_msg(can_message, pdMS_TO_TICKS(1000)) == ESP_OK) {
             ESP_LOGI(LOG_TAG, "Message received: ID=0x%03x, DLC=%d", can_message.identifier, can_message.data_length_code);
             ESP_LOG_BUFFER_HEXDUMP(LOG_TAG, can_message.data, can_message.data_length_code, ESP_LOG_INFO);
-            
+
             dash_update_int(&g_leaf_dash.voltage, can_message.data[0]);
             dash_update_int(&g_leaf_dash.current, can_message.data[1]);
             dash_update_int(&g_leaf_dash.soc, can_message.data[2]);
